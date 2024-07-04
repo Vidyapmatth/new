6575071adbe6e2aa3d9a72e19513f5468d2727bd
Vidyapmatth <161805638+Vidyapmatth@users.noreply.github.com>
2024-07-04 11:32:40 +0530

    Update CreateCampaignTest.java

--- Vitiger/src/test/java/Campaign/CreateCampaignTest.java
+++ Vitiger/src/test/java/Campaign/CreateCampaignTest.java
@@ -26,6 +26,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class CreateCampaignTest extends BaseClass {
+	//first push 
+	
 //@Test(groups ="SystemTesting")
 //@Test(retryAnalyzer = Generic_Utilities.RetryImplementation.class)
 @Test
